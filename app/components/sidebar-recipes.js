a44745b3c34c66a8f699547eae8fa164ddc5b6d2
Daniel Wulff <danielwulff.ee@gmail.com>
2016-06-17 17:56:53 -0700

    favorite list working properly (using a service)

--- app/components/sidebar-recipes.js
+++ app/components/sidebar-recipes.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favorites: Ember.inject.service(),
-  actions: {
-    favorite(recipe) {
-      this.get('favorites').add(recipe);
-    }
-  }
+
 });
--- app/services/favorites.js
+++ app/services/favorites.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  items: [],
+
+  add(addedItem) {
+    var found = false;
+    this.get('items').forEach(function(instance) {
+      if (instance === addedItem) {
+        found = true;
+        return;
+      }
+    })
+    if(!found) {
+      this.get('items').pushObject(addedItem);
+    }
+  }
 });
